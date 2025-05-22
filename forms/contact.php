c18bdc4081f681e3f106cb0820fffef9da06d61d
SamuelNgundi <samuelwngundi02@gmail.com>
2025-05-22 22:07:08 +0300

    Schedule

--- forms/contact.php
+++ forms/contact.php
@@ -30,5 +30,6 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send();\\
+  echo $contact -> add_message( $_POST['name'], 'From');
 ?>
