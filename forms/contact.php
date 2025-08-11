20ff5e94ad628efc76da1adbbc7abf1f65bc9ce4
SamuelNgundi <samuelwngundi02@gmail.com>
2025-08-11 23:49:10 +0300

    Space it out

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,28 @@
-<!-- <?php -
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https:
-  */
-
-  
+<?php
+  // Email address to receive the contact form submissions
   $receiving_email_address = 'fogccasociety@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  // Check if the PHP Email Form library exists and include it
+  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
+  // Set form data
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  
- 
+  // Add messages
+  $contact->add_message($_POST['name'], 'From');
+  $contact->add_message($_POST['email'], 'Email');
+  $contact->add_message($_POST['message'], 'Message', 10);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
+  // Send email and output result
   echo $contact->send();
-  echo $contact -> add_message( $_POST['name'], 'From');
 ?>
